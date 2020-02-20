3f4c8d534277892620fe2bea859ee8849b58dbd8
Nallamothu <anallam@anallam-mjg5j.local>
2020-02-20 15:42:55 +0530

    Updates

---
+++ precache-manifest.2ff3acc5971524a1173281f81b40e7ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dc3db168daaa7974fee485fd3cfbfbc",
+    "url": "/movie-application/index.html"
+  },
+  {
+    "revision": "1be8201aaf96819f5028",
+    "url": "/movie-application/static/css/main.6c0353a0.chunk.css"
+  },
+  {
+    "revision": "e18b9dd1c48b27e3e317",
+    "url": "/movie-application/static/js/2.654d20dd.chunk.js"
+  },
+  {
+    "revision": "1be8201aaf96819f5028",
+    "url": "/movie-application/static/js/main.6c4eed8c.chunk.js"
+  },
+  {
+    "revision": "d3df3e351e1c8370923f",
+    "url": "/movie-application/static/js/runtime~main.8bae456b.js"
+  }
+]);
